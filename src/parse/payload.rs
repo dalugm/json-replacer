f46266f5abe4857fcbbd487f573a732dd9830ff6
dalu <mou.tong@outlook.com>
2025-07-30 14:42:26 +0800

    Avoid hardcoded quote

--- src/parse/payload.rs
+++ src/parse/payload.rs
@@ -53,7 +53,7 @@ fn parse_search_query_group(
     group: SearchQueryGroup,
     hashmap: &HashMap<String, ObjectAttribute>,
 ) -> String {
-    let mut expr = "(".to_owned();
+    let mut lisp_expr_vec: Vec<String> = Vec::new();
 
     let operator = match group.operator {
         SearchQueryGroupOperator::And => "AND",
@@ -61,125 +61,115 @@ fn parse_search_query_group(
         SearchQueryGroupOperator::Not => "NOT",
     };
 
-    expr.push_str(operator);
+    lisp_expr_vec.push(operator.to_string());
 
     if let Some(conditions) = group.search_query_conditions {
         let cond_expr = parse_search_query_conditions(conditions, hashmap);
-        expr.push_str(&cond_expr);
+        lisp_expr_vec.push(cond_expr);
     }
 
     if let Some(children) = group.children {
-        let cond_expr = parse_search_query_children(children, hashmap);
-        expr.push_str(&cond_expr);
+        let children_expr = parse_search_query_children(children, hashmap);
+        lisp_expr_vec.push(children_expr);
     }
 
-    expr.push(')');
+    let lisp_expr = format!("({})", lisp_expr_vec.join(" "));
 
-    expr
+    lisp_expr
 }
 
 fn parse_search_query_children(
     children: Vec<SearchQueryGroup>,
     hashmap: &HashMap<String, ObjectAttribute>,
 ) -> String {
-    let mut expr = "(".to_owned();
-
-    for child in children {
-        expr.push_str(&parse_search_query_group(child, hashmap));
-    }
-
-    expr.push(')');
-
-    expr
+    children
+        .into_iter()
+        .map(|child| parse_search_query_group(child, hashmap))
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 fn parse_search_query_conditions(
     conditions: Vec<SearchQueryCondition>,
     hashmap: &HashMap<String, ObjectAttribute>,
 ) -> String {
-    let mut expr = "(".to_owned();
+    let lisp_exprs: Vec<String> = conditions
+        .into_iter()
+        .map(|condition| {
+            let operator = match condition.operator {
+                SearchQueryConditionOperator::Equal => "equal",
+                SearchQueryConditionOperator::NotEqual => "not_equal",
+                SearchQueryConditionOperator::Contain => "contain",
+                SearchQueryConditionOperator::NotContain => "not_contain",
+                SearchQueryConditionOperator::IsPresent => "is_present",
+                SearchQueryConditionOperator::IsBlank => "is_blank",
+                SearchQueryConditionOperator::Greater => "greater_than",
+                SearchQueryConditionOperator::GreaterOrEqual => "greater_than_equal",
+                SearchQueryConditionOperator::Less => "less_than",
+                SearchQueryConditionOperator::LessOrEqual => "less_than_equal",
+                SearchQueryConditionOperator::Between => "between",
+                SearchQueryConditionOperator::Today => "today",
+                SearchQueryConditionOperator::BeforeToday => "before_today",
+                SearchQueryConditionOperator::AfterToday => "after_today",
+                SearchQueryConditionOperator::ThisWeek => "this_week",
+                SearchQueryConditionOperator::BeforeThisWeek => "before_this_week",
+                SearchQueryConditionOperator::AfterThisWeek => "after_this_week",
+                SearchQueryConditionOperator::ThisMonth => "this_month",
+                SearchQueryConditionOperator::BeforeThisMonth => "before_this_month",
+                SearchQueryConditionOperator::AfterThisMonth => "after_this_month",
+                SearchQueryConditionOperator::ThisQuarter => "this_quarter",
+                SearchQueryConditionOperator::BeforeThisQuarter => "before_this_quarter",
+                SearchQueryConditionOperator::AfterThisQuarter => "after_this_quarter",
+                SearchQueryConditionOperator::ThisYear => "this_year",
+                SearchQueryConditionOperator::BeforeThisYear => "before_this_year",
+                SearchQueryConditionOperator::AfterThisYear => "after_this_year",
+                SearchQueryConditionOperator::AnyOf => "any_of",
+                SearchQueryConditionOperator::NoneOf => "none_of",
+                SearchQueryConditionOperator::IsTrue => "is_true",
+                SearchQueryConditionOperator::IsFalse => "is_false",
+                SearchQueryConditionOperator::Address => "address",
+            };
 
-    for condition in conditions {
-        expr.push('(');
-        let operator = match condition.operator {
-            SearchQueryConditionOperator::Equal => "equal",
-            SearchQueryConditionOperator::NotEqual => "not_equal",
-            SearchQueryConditionOperator::Contain => "contain",
-            SearchQueryConditionOperator::NotContain => "not_contain",
-            SearchQueryConditionOperator::IsPresent => "is_present",
-            SearchQueryConditionOperator::IsBlank => "is_blank",
-            SearchQueryConditionOperator::Greater => "greater_than",
-            SearchQueryConditionOperator::GreaterOrEqual => "greater_than_equal",
-            SearchQueryConditionOperator::Less => "less_than",
-            SearchQueryConditionOperator::LessOrEqual => "less_than_equal",
-            SearchQueryConditionOperator::Between => "between",
-            SearchQueryConditionOperator::Today => "today",
-            SearchQueryConditionOperator::BeforeToday => "before_today",
-            SearchQueryConditionOperator::AfterToday => "after_today",
-            SearchQueryConditionOperator::ThisWeek => "this_week",
-            SearchQueryConditionOperator::BeforeThisWeek => "before_this_week",
-            SearchQueryConditionOperator::AfterThisWeek => "after_this_week",
-            SearchQueryConditionOperator::ThisMonth => "this_month",
-            SearchQueryConditionOperator::BeforeThisMonth => "before_this_month",
-            SearchQueryConditionOperator::AfterThisMonth => "after_this_month",
-            SearchQueryConditionOperator::ThisQuarter => "this_quarter",
-            SearchQueryConditionOperator::BeforeThisQuarter => "before_this_quarter",
-            SearchQueryConditionOperator::AfterThisQuarter => "after_this_quarter",
-            SearchQueryConditionOperator::ThisYear => "this_year",
-            SearchQueryConditionOperator::BeforeThisYear => "before_this_year",
-            SearchQueryConditionOperator::AfterThisYear => "after_this_year",
-            SearchQueryConditionOperator::AnyOf => "any_of",
-            SearchQueryConditionOperator::NoneOf => "none_of",
-            SearchQueryConditionOperator::IsTrue => "is_true",
-            SearchQueryConditionOperator::IsFalse => "is_false",
-            SearchQueryConditionOperator::Address => "address",
-        };
-        expr.push_str(operator);
+            let (name, value) = match hashmap.get(&condition.object_attribute_id) {
+                Some(oa) => {
+                    let name = oa.attributes.name.clone();
+                    let mut value = condition.value;
 
-        let (name, value) = match hashmap.get(&condition.object_attribute_id) {
-            Some(oa) => {
-                let name = oa.attributes.name.clone();
-                let mut value = condition.value;
+                    if oa.attributes.data_type == "picklist" {
+                        value = value.map(|value| process_picklist_oa_value(oa, value));
+                    }
 
-                if oa.attributes.data_type == "picklist" {
-                    value = match value {
-                        Some(value) => Some(process_picklist_oa_value(oa, value)),
-                        None => None,
-                    };
+                    (name, value)
                 }
+                None => ("not found".to_string(), condition.value),
+            };
 
-                (name, value)
-            }
-            None => ("not found".to_string(), condition.value),
-        };
+            let args = match value {
+                Some(value) => &format!("{name} {value}"),
+                None => &name,
+            };
 
-        let str = match value {
-            Some(value) => format!(" {name} {value}"),
-            None => format!(" {name}"),
-        };
+            let lisp_expr = format!("({})", [operator, args].join(" "));
 
-        expr.push_str(&str);
+            lisp_expr
+        })
+        .collect();
 
-        expr.push(')');
-    }
-
-    expr.push(')');
-
-    expr
+    lisp_exprs.join(" ").to_string()
 }
 
 fn parse_search_query(
     search_query: SearchQuery,
     hashmap: &HashMap<String, ObjectAttribute>,
 ) -> Result<String> {
-    let mut expr = "".to_owned();
+    let lisp_expr = search_query
+        .search_query_groups
+        .into_iter()
+        .map(|group| parse_search_query_group(group, hashmap))
+        .collect::<Vec<String>>()
+        .join(" ");
 
-    for group in search_query.search_query_groups {
-        let str = parse_search_query_group(group, hashmap);
-        expr.push_str(&str);
-    }
-
-    Ok(expr)
+    Ok(lisp_expr)
 }
 
 pub fn parse_payload(
@@ -192,16 +182,20 @@ pub fn parse_payload(
 
     let mut map = HashMap::new();
 
-    let names = payload
+    let search_oa_list = payload
         .object_attribute_ids
         .into_iter()
-        .map(|id| match hashmap.get(&id) {
-            Some(oa) => oa.attributes.name.clone(),
-            None => "not found".to_string(),
+        .map(|id| {
+            let name = match hashmap.get(&id) {
+                Some(oa) => oa.attributes.name.clone(),
+                None => "not found".to_string(),
+            };
+
+            format!("{name}, {id}")
         })
         .collect();
 
-    map.insert("names".to_string(), names);
+    map.insert("object_attributes".to_string(), search_oa_list);
 
     let search_query = parse_search_query(payload.search_query, hashmap)?;
 
